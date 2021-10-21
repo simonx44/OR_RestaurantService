fa00a87a5b9219854c9b6c883a1f3ef30907e152
Simon Schwegler <you@example.comgit>
2021-10-21 11:53:40 +0200

    client

---
+++ client/custom_function.js
@@ -0,0 +1,27 @@
+function testFunktion(data) {
+
+    console.log(data);
+
+    console.log("teststststst");
+
+    console.log("custome js function")
+
+
+    const containerId = "#container-shoppinglist"
+
+    $(containerId).empty();
+    $(containerId).append("<div><h1>Test hat funktioniert</h1></div>")
+
+}
+
+
+
+
+function updateOrder(data) {
+
+
+
+
+
+
+}
